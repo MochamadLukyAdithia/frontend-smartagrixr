c01312d218b019ede706a1ff400e204f0314877a
MochamadLukyAdithia <mochamadlukyadithiawork@gmail.com>
2026-09-11 23:22:06 +0700

    update api

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 export const API_URL = (
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000"
+  process.env.NEXT_PUBLIC_API_URL || "https://backend-smartagrixr-production.up.railway.app"
 ).replace(/\/+$/, "");
 
 export interface User {
